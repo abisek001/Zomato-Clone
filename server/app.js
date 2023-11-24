8e05edccdab43164d81e6c18a3c30735bf9db6b0
abisek001 <abisek501@gmail.com>
2023-11-24 13:28:04 +0530

    cros origin url removed

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
  });
 
 const corsOptions = {
-   origin: ['http://localhost:3000', "https://mern-app-zomato.onrender.com"],
+   origin: ['http://localhost:3000'],
    credentials: true,
    optionSuccessStatus:200
 }
