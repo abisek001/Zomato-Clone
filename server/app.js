cbc189ebae4b2b733c5fa14873aed64162707175
abisek001 <abisek501@gmail.com>
2023-11-24 13:10:35 +0530

    deploye zomato project

---
+++ server/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const cookieSession = require('cookie-session');
+const router = require('./Router/index');
+const dotenv = require('dotenv');
+const paymentRoutes = require('./Controllers/payment');
+const passport = require('passport');
+const passportSetup = require('./Controllers/passport');
+const authRote = require('./Controllers/auth');
+
+const hostname = "localhost";
+const port = "5502";
+
+app.use(cookieSession({ name:"Session", keys:["edureka"], makAge: 24*60*60*1000 }));
+
+app.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+   res.header('Access-Control-Allow-Credentials', 'true');
+   next();
+ });
+
+const corsOptions = {
+   origin: ['http://localhost:3000', "https://mern-app-zomato.onrender.com"],
+   credentials: true,
+   optionSuccessStatus:200
+}
+dotenv.config();
+app.use(cors(corsOptions));
+app.use(bodyparser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/',router);
+app.use('/api/payment', paymentRoutes)
+app.use('/auth', authRote);
+
+
+const dbUrl = 'mongodb://127.0.0.1:27017/restaurant';
+
+const atlesdbUrl = process.env.atlesdbUrl
+mongoose.connect(atlesdbUrl,{useNewUrlParser: true, useUnifiedTopology: true})
+.then( res => {
+   app.listen(port, hostname, () => {
+      console.log(`Server is runnung on http://${hostname}:${port}`);
+   });   
+})
+.catch(err =>console.log(err));
+
